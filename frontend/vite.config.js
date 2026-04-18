f63d0fb798c37e0f9def19098fc6e9b6fc6b2983
TejasTS2006 <tv03082@gmail.com>
2026-04-18 23:34:06 +0530

    Second

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'httphttps://studentmanagement-zv5g.onrender.com://localhost:5000',
         changeOrigin: true,
       },
     },
